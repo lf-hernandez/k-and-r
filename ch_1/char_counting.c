8a7da2aee86f97c0586bcce750c26a6ee745fb20
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2024-05-28 18:58:40 -0400

    Add counting examples from 1.5.2 and 1.5.3
    Signed-off-by: Luis Felipe Hernandez <luis.hernandez093@gmail.com>

---
+++ ch_1/char_counting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	long nc;
+	/*
+	 * nc = 0;
+	 * while (getchar() != EOF)
+	 *	++nc;
+	 */
+
+	for (nc = 0; getchar() != EOF; ++nc)
+		;
+	printf("%ld\n", nc);
+
+	return 0;
+}
